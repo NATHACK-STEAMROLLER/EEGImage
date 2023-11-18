9304ee35dbcd3767b56f44e525aa6953a497df1f
MingbaoF <mingbao@ualberta.ca>
2023-11-17 23:57:45 -0700

    ui and corresponding python file for BrainBrushPage and featuresPage are added

---
+++ EEGImage/EEGImage/featuresPage.py
@@ -0,0 +1,25 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout
+from PyQt6.QtGui import QMovie
+from PyQt6.uic import loadUi
+import sys
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Load the UI file
+        loadUi('featuresPage.ui', self)
+
+        # Assuming you have a QLabel in your UI for displaying the GIF
+        self.label = QLabel(self.centralWidget())
+        self.label.setGeometry(0, 0, 800, 600)  # Set the dimensions as per your requirements
+
+
+        # Start the movie
+        #self.movie.start()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec())
