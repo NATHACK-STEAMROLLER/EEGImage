b86ec0504003eb0fb73d961d6fe9dcac041ff513
Louis Ye <y1577262850@gmail.com>
2023-11-19 07:59:02 -0700

    html update

--- EEGImage/generateImage/urls.py
+++ EEGImage/generateImage/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     # path('generate', views.generate, name='generate'),
     path('', views.start_page, name='start_page'),
     path('countdown/', views.countdown_page, name='countdown_page'),
+    path('image_display/', views.image_display, name='image_display'),
+    path('image_processing/', views.image_processing, name='image_processing'),
+
     path('history/', views.history, name='history'),
 ]
