52340a50db44f0752adec2cd9a32b46f16c13fa2
andy-yu-ua <zixu3@ualberta.ca>
2023-11-18 17:13:03 -0700

    more UI added

--- EEGImage/generateImage/urls.py
+++ EEGImage/generateImage/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('generate', views.generate, name='generate'),
+    # path('', views.index, name='index'),
+    # path('generate', views.generate, name='generate'),
+    path('', views.start_page, name='start_page'),
+    path('countdown/', views.countdown_page, name='countdown_page'),
 ]
--- EEGImage/generateImage/views.py
+++ EEGImage/generateImage/views.py
@@ -2,24 +2,42 @@ from django.http import HttpResponse
 from django.template import loader
 import requests
 
-# Create your views here.
-def index(request):
-    # Render the index.html template
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render(None, request))
+# # Create your views here.
+# def index(request):
+#     # Render the index.html template
+#     template = loader.get_template('index.html')
+#     return HttpResponse(template.render(None, request))
     
-def generate(request):
-    # Get the input string from the form
-    input_string = request.POST.get('input_string')
-    # Call the graphic_art tool with the input string
+# def generate(request):
+#     # Get the input string from the form
+#     input_string = request.POST.get('input_string')
+#     # Call the graphic_art tool with the input string
 
-    # response = requests.post('https://bing.com/graphic_art', data={'prompt': input_string})
-    # Get the image url from the response
-    # image_url = response.json().get('image_url')
+#     # response = requests.post('https://bing.com/graphic_art', data={'prompt': input_string})
+#     # Get the image url from the response
+#     # image_url = response.json().get('image_url')
 
-    # Render the generate.html template with the image url
-    template = loader.get_template('generate.html')
-    # image url should be the url of the image returned from the graphic_art tool
-    context = {'image_url': 'https://picsum.photos/id/237/200/300',
-               'text' : input_string}
-    return HttpResponse(template.render(context, request))
+#     # Render the generate.html template with the image url
+#     template = loader.get_template('generate.html')
+#     # image url should be the url of the image returned from the graphic_art tool
+#     context = {'image_url': 'https://picsum.photos/id/237/200/300',
+#                'text' : input_string}
+#     return HttpResponse(template.render(context, request))
+
+
+from django.shortcuts import render
+import random
+import time
+
+def start_page(request):
+    return render(request, 'start_page.html')
+
+def countdown_page(request):
+    # Simulate a 5-second countdown
+    # for i in range(5, 0, -1):
+    #     time.sleep(1)
+
+    # Generate a random image (replace this with your logic)
+    random_image_url = "https://picsum.photos/id/237/200/300"  # Example URL
+
+    return render(request, 'countdown_page.html', {'random_image_url': random_image_url})
